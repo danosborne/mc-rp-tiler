b4f0659e755b1f3166df55f1add3641f9d0cda86
Dan Osborne <danosborne@gmail.com>
2023-05-28 22:10:19 +0100

    imagemagick was glitchy, did an OK job but not perfect, even only running each job synchronously

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
 const walk = require('walkdir');
-const {spawn} = require('node:child_process');
+const {spawnSync} = require('node:child_process');
 const fs = require('fs');
 const path = require('path');
+const Jimp = require("jimp");
+
+const tileFile = (src, target) => {
+    const buffer = Buffer.alloc(32 * 32 *4);
+    new Jimp({ data: buffer, width: 32, height: 32 }, (err, image) => {
+        if (err) console.error(err);
+        
+        Jimp.read(src, (err, texture) => {
+            if (err) throw err;
+        
+            const d = texture.bitmap.data;
+            for (let x = 0; x < 16 * 2; x++) {
+                for (let y = 0; y < 16; y++) {
+                    image.setPixelColor(texture.getPixelColor(x % 16, y), x, y);
+                }   
+            }
+            for (let x = 0; x < 16 * 2; x++) {
+                for (let y = 0; y < 16; y++) {
+                    image.setPixelColor(texture.getPixelColor(x % 16, y), x, 16 + y);
+                }   
+            }
+
+            image.write(target);
+        });    
+    });
+}
 
 const target = (src) => src.replace(/\/images\//, '/tiled-images/');
-const spawnCommand = (src) => {
-    const input = `tile:${src}`;
-    const output = target(src);
-    return spawn(
-    //   'convert', [
-    //     '-size',
-    //     '32x32',
-    //     input,
-    //     output
-    //     ],
-    `magick -size 32x32 ${input} ${output}`,[],
-      {
-        stdio: 'inherit',
-        shell: true,
-      }
-    );
-};
 
-const WALK_DIR = `${__dirname}/images/`;
-const emitter = walk(WALK_DIR);
-emitter.on('file', function(filename, stat) {
+const handleFile = (filename) => {
     if (filename.endsWith('.png')) {
         const targetFilePath = target(filename);
         const dirname = path.dirname(targetFilePath);
         if (!fs.existsSync(dirname)){
             fs.mkdirSync(dirname, { recursive: true });
         }
-        const cp = spawnCommand(filename);
-        cp.on('error', (err) => {
-            console.error(err);
-        });
+        tileFile(filename, targetFilePath);
     }
-});
- 
+};
+
+const WALK_DIR = `${__dirname}/images/`;
+const paths = walk.sync(WALK_DIR);
+paths.forEach(handleFile); 
+
